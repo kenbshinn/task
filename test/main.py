ff1f67b0ab65df1de8a8dfb3acbce8022174074e
Ken Shinn <kenbshinn@gmail.com>
2021-12-30 13:05:35 -0500

    Added Edit profile page and DB lookup

--- test/main.py
+++ test/main.py
@@ -131,5 +131,18 @@ def profile():
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
+# http://localhost:5000/pythinlogin/edit_profile- this will be the edit profile page, only accessible for loggedin users
+@app.route('/pythonlogin/edit_profile')
+def edit_profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('edit_profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 app.run(debug=True)
